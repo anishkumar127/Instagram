296f7b83c2f76e473e17f409ce2a266ceb166bf4
anishkumar127 <77631750+anishkumar127@users.noreply.github.com>
2021-03-10 11:57:40 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  MONGOURI:
-    "mongodb+srv://anishkumar:anishking@cluster0.ybty6.mongodb.net/Instagram?retryWrites=true&w=majority",
-  JWT_SECRET: "eDefadDEakOOKlkdSD32SDsd",
-  Email_USERNAME: "anishbishnoi127@gmail.com",
-  Email_PASSWORD: "jI6r2PyZJA8bGL7v",
-  EMAIL: "http:/localhost:3001",
-};
